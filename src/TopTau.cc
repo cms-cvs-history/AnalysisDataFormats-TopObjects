2a45312bcbd15c524e2ddb9852ff284fa46249b5
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-10-31 22:46:23 +0000

    Made TopTau a separated class with additional members.

---
+++ src/TopTau.cc
@@ -0,0 +1,21 @@
+//
+// $Id: TopTau.cc,v 1.1 2007/10/30 09:59:06 delaer Exp $
+//
+
+#include "AnalysisDataFormats/TopObjects/interface/TopTau.h"
+
+
+/// default constructor
+TopTau::TopTau() : TopLepton<TopTauType>(),emEnergyFraction_(0.),eOverP_(0.) {
+}
+
+
+/// constructor from TopTauType
+TopTau::TopTau(const TopTauType & aTau):TopLepton<TopTauType>(aTau),emEnergyFraction_(0.),eOverP_(0.) {
+}
+
+
+/// destructor
+TopTau::~TopTau() {
+}
+
