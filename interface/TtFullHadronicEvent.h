504340e72195bd34f519b0887c0a4aa5b0651eb9
Eike Schlieckau <sha1-fc2fe05d7bf2eff84264ececb7f974ab67c284cc@cern.ch>
2009-10-29 13:52:51 +0000

    Added full hadronic class

---
+++ interface/TtFullHadronicEvent.h
@@ -0,0 +1,77 @@
+#ifndef TopObjects_TtFullHadronicEvent_h
+#define TopObjects_TtFullHadronicEvent_h
+
+#include "AnalysisDataFormats/TopObjects/interface/TtEvent.h"
+
+namespace TtFullLepDaughter{
+  /// full leptonic daughter names for common
+  /// use and use with the hypotheses
+  static const std::string Nu   ="Nu"   , LepBar="LepBar", WPlus ="WPlus" , B   ="B"   , Top   ="Top";
+  static const std::string NuBar="NuBar", Lep   ="Lep"   , WMinus="WMinus", BBar="BBar", TopBar="TopBar"; 
+}
+
+/**
+   \class   TtFullHadronicEvent TtFullHadronicEvent.h "AnalysisDataFormats/TopObjects/interface/TtFullHadronicEvent.h"
+
+   \brief   Class derived from the TtEvent for the full hadronic decay channel
+
+   The structure holds information on the hadronic decay channels, 
+   all event hypotheses of different classes (user defined during
+   production) and a reference to the TtGenEvent (if available). It 
+   provides access and administration.
+*/
+
+class TtFullHadronicEvent: public TtEvent {
+  
+ public:
+  /// empty constructor
+  TtFullHadronicEvent(){};
+  /// default destructor
+  virtual ~TtFullHadronicEvent(){};
+
+  /// get top of the given hypothesis
+  const reco::Candidate* top(const std::string& key, const unsigned& cmb=0) const { return top(hypoClassKeyFromString(key), cmb); };
+  /// get top of the given hypothesis
+  const reco::Candidate* top(const HypoClassKey& key, const unsigned& cmb=0) const { return !isHypoValid(key,cmb) ? 0 : eventHypo(key,cmb). daughter(TtFullLepDaughter::Top); };
+  /// get b of the given hypothesis
+  const reco::Candidate* b(const std::string& key, const unsigned& cmb=0) const { return b(hypoClassKeyFromString(key), cmb); };
+  /// get b of the given hypothesis
+  const reco::Candidate* b(const HypoClassKey& key, const unsigned& cmb=0) const { return !isHypoValid(key,cmb) ? 0 : top(key,cmb)->daughter(TtFullLepDaughter::B); };
+  /// get Wplus of the given hypothesis
+  const reco::Candidate* wPlus(const std::string& key, const unsigned& cmb=0) const { return wPlus(hypoClassKeyFromString(key), cmb); };
+  /// get Wplus of the given hypothesis
+  const reco::Candidate* wPlus(const HypoClassKey& key, const unsigned& cmb=0) const { return !isHypoValid(key,cmb) ? 0 : top(key,cmb)->daughter(TtFullLepDaughter::WPlus); };
+
+  /// get anti-top of the given hypothesis
+  const reco::Candidate* topBar(const std::string& key, const unsigned& cmb=0) const { return topBar(hypoClassKeyFromString(key), cmb); };
+  /// get anti-top of the given hypothesis
+  const reco::Candidate* topBar(const HypoClassKey& key, const unsigned& cmb=0) const { return !isHypoValid(key,cmb) ? 0 : eventHypo(key,cmb). daughter(TtFullLepDaughter::TopBar); };
+  /// get anti-b of the given hypothesis
+  const reco::Candidate* bBar(const std::string& key, const unsigned& cmb=0) const { return bBar(hypoClassKeyFromString(key), cmb); };
+  /// get anti-b of the given hypothesis
+  const reco::Candidate* bBar(const HypoClassKey& key, const unsigned& cmb=0) const { return !isHypoValid(key,cmb) ? 0 : topBar(key,cmb)->daughter(TtFullLepDaughter::BBar  ); };
+  /// get Wminus of the given hypothesis
+  const reco::Candidate* wMinus(const std::string& key, const unsigned& cmb=0) const { return wMinus(hypoClassKeyFromString(key), cmb); };
+  /// get Wminus of the given hypothesis
+  const reco::Candidate* wMinus(const HypoClassKey& key, const unsigned& cmb=0) const { return !isHypoValid(key,cmb) ? 0 : topBar(key,cmb)->daughter(TtFullLepDaughter::WMinus); };
+
+  /// get top of the TtGenEvent
+  const reco::GenParticle* top        () const { return (!genEvt_ ? 0 : this->genEvent()->top()        ); };
+  /// get b of the TtGenEvent
+  const reco::GenParticle* b          () const { return (!genEvt_ ? 0 : this->genEvent()->b()          ); };
+  /// get Wplus of the TtGenEvent
+  const reco::GenParticle* wPlus      () const { return (!genEvt_ ? 0 : this->genEvent()->wPlus()      ); };
+
+  /// get anti-top of the TtGenEvent
+  const reco::GenParticle* topBar     () const { return (!genEvt_ ? 0 : this->genEvent()->topBar()     ); };
+  /// get anti-b of the TtGenEvent
+  const reco::GenParticle* bBar       () const { return (!genEvt_ ? 0 : this->genEvent()->bBar()       ); };
+  /// get Wminus of the TtGenEvent
+  const reco::GenParticle* wMinus     () const { return (!genEvt_ ? 0 : this->genEvent()->wMinus()     ); };
+
+  /// print full content of the structure as formated 
+  /// LogInfo to the MessageLogger output for debugging  
+  void print();
+};
+
+#endif
